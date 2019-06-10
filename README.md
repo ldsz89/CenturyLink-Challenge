30956e0e801abcf51ff3ea8688176c93ed5952ab
Dre Shannnon <ldsz89@mail.missouri.edu>
2019-06-09 20:36:51 -0500

    Added README and moved updated finished endpoint from test endpoint

---
+++ README.md
@@ -0,0 +1,36 @@
+# CentryLink Challenge
+This API was written using Python and Flask on Windows 10. To run it, first install flask.
+```
+> pip install Flask
+> // or
+> py -m pip install Flask
+```
+
+## Virtual Environment
+Next, in the project folder, we'll want to set up a virtual environment to run the application.
+```
+> py -m venv venv
+```
+To set the environment variables on Windows:
+```
+> set FLASK_ENV=development
+> set FLASK_APP=app.py
+```
+Or on Linux:
+```
+$ export FLASK_ENV=development
+$ export FLASK_APP=app.py
+```
+
+## Run it!
+Now to run the app:
+```
+> flask run
+```
+Note: this opened on port 5000 for me.
+
+## Endpoint
+This endpoint was tested using Postman. To get the relevent data, use endpoint:
+```
+localhost:5000/user/<username>
+```
--- app.py
+++ app.py
@@ -13,20 +13,12 @@ def say_hello():
 
 @app.route('/user/<username>')
 def retrieve_user_info(username):
-    user = {}
-    followers_url = '/users/' + username + '/followers'
-    r = requests.get(url = URL + followers_url)
-    data = r.json()
-    if len(data) > 0:
-        user['followers'] = []
-        for follower in data:
-            new_follower = {}
-            new_follower['user'] = follower['login']
-            new_follower['followers'] = {}
-            user['followers'].append(new_follower)
-    else:
-        user['followers'] = 'User has 0 followers'
-    return str(user)
+    user = get_followers(username, 5)
+    user['user'] = username
+    if len(user['followers']) > 0:
+        for follower in user['followers']:
+            follower['followers'] = get_followers(follower['user'], 5)
+    return jsonify(user)
 
 @app.route('/test/user/<username>')
 def test_user_info(username):
